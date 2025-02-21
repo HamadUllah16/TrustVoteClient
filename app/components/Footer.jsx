b9d96e367f8619bb3f2767e3be990e32900ca811
Hamad Ullah <hamadullah16@gmail.com>
2025-02-21 19:42:36 +0500

    landing page redesigned and made responsive

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { Grid, IconButton, Typography } from '@mui/material'
+import { Grid, IconButton, Stack, Typography } from '@mui/material'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -22,88 +22,85 @@ function Footer() {
 
                     null
                     :
-                    <Grid
+                    <Stack
                         backgroundColor={"background"}
-                        px={"75px"}
-                        py={"30px"}
                         bgcolor={'secondary.main'}
+                        className='px-20 py-7 flex-row'
                     >
-                        <Grid
+
+                        <Stack
                             display={"flex"}
-                            flexDirection={"column"}
-                            justifyContent={'center'}
-                            gap={4}
-                            px={3}
-                            py={4}
+                            justifyContent={"space-between"}
+                            className='
+                                md:justify-start md:flex-row
+                                sm:justify-center sm:flex-col sm:gap-5
+                                max-sm:justify-center max-sm:flex-col max-sm:gap-5
+                                w-full
+                                '
                         >
-
-                            <Grid
-                                display={"flex"}
-                                justifyContent={"space-between"}
-                            >
-                                <Grid item={1} display={"flex"} alignItems={"center"}>
-                                    <Typography
-                                        variant='h4'
-                                        fontWeight={"bolder"}
-                                        color={'primary.main'}
-                                        p={1}
-                                    >
-                                        Trust Vote
-                                    </Typography>
+                            <Stack className={'md:w-[25%] sm:w-full max-sm:w-full '}>
+                                <Typography
+                                    variant='h4'
+                                    fontWeight={"bolder"}
+                                    color={'primary.main'}
+                                    p={1}
+                                    className='md:text-start sm:text-center max-sm:text-center'
+                                >
+                                    Trust Vote
+                                </Typography>
+                            </Stack>
+                            <Stack className={'md:w-[25%] sm:w-full max-sm:w-full '}>
+                                <Typography variant='h5' color={'secondary.100'}>About</Typography>
+                                <Grid
+                                >
+                                    <Link href={"#privacypolicy"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>Privacy Policy</Typography>
+                                    </Link>
+                                    <Link href={"#contact"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>Contact</Typography>
+                                    </Link>
+                                    <Link href={"#careers"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>Careers</Typography>
+                                    </Link>
+                                    <Link href={"terms"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>Terms of Service</Typography>
+                                    </Link>
                                 </Grid>
-                                <Grid item={1} display={"flex"} flexDirection={"column"} gap={"30px"}>
-                                    <Typography variant='h5' color={'secondary.100'}>About</Typography>
-                                    <Grid
-                                    >
-                                        <Link href={"#privacypolicy"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>Privacy Policy</Typography>
-                                        </Link>
-                                        <Link href={"#contact"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>Contact</Typography>
-                                        </Link>
-                                        <Link href={"#careers"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>Careers</Typography>
-                                        </Link>
-                                        <Link href={"terms"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>Terms of Service</Typography>
-                                        </Link>
-                                    </Grid>
-                                </Grid>
+                            </Stack>
 
-                                <Grid item={1} display={"flex"} flexDirection={"column"} gap={"30px"}>
-                                    <Typography variant='h5' color={'secondary.100'}>Help & Support</Typography>
-                                    <Grid
-                                    >
-                                        <Link href={"#faqs"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>FAQs</Typography>
-                                        </Link>
-                                        <Link href={"#email"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>help@trustvote.com</Typography>
-                                        </Link>
-                                        <Link href={"#phone"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>+921234567890</Typography>
-                                        </Link>
-                                    </Grid>
+                            <Stack className={'md:w-[25%] sm:w-full max-sm:w-full '}>
+                                <Typography variant='h5' color={'secondary.100'}>Help & Support</Typography>
+                                <Grid
+                                >
+                                    <Link href={"#faqs"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>FAQs</Typography>
+                                    </Link>
+                                    <Link href={"#email"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>help@trustvote.com</Typography>
+                                    </Link>
+                                    <Link href={"#phone"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>+921234567890</Typography>
+                                    </Link>
                                 </Grid>
+                            </Stack>
 
-                                <Grid item={1} display={"flex"} flexDirection={"column"} gap={"30px"}>
-                                    <Typography variant='h5' color={'secondary.100'}>Account</Typography>
-                                    <Grid
-                                    >
-                                        <Link href={firstName ? '/user/dashboard' : "/login"}>
-                                            <Typography variant='subtitle1' color={"#5A5A5A"}>{firstName ?? 'Login'}</Typography>
+                            <Stack className={'md:w-[25%] sm:w-full max-sm:w-full '}>
+                                <Typography variant='h5' color={'secondary.100'}>Account</Typography>
+                                <Grid
+                                >
+                                    <Link href={firstName ? '/user/dashboard' : "/login"}>
+                                        <Typography variant='subtitle1' color={"#5A5A5A"}>{firstName ?? 'Login'}</Typography>
+                                    </Link>
+                                    {firstName ? null :
+                                        <Link href={"/register"}>
+                                            <Typography variant='subtitle1' color={"#5A5A5A"}>Register</Typography>
                                         </Link>
-                                        {firstName ? null :
-                                            <Link href={"/register"}>
-                                                <Typography variant='subtitle1' color={"#5A5A5A"}>Register</Typography>
-                                            </Link>
-                                        }
-                                    </Grid>
+                                    }
                                 </Grid>
+                            </Stack>
 
-                            </Grid>
-                        </Grid>
-                    </Grid>
+                        </Stack>
+                    </Stack>
             }
         </>
     )
